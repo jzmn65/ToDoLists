4322d379279bace563659958f0598b50015bd16a
Jazmine Singh <jzmn65@gmail.com>
2025-10-15 13:09:26 -0400

    Initial Commit

---
+++ ToDoLists/ToDoListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListsApp.swift
+//  ToDoLists
+//
+//  Created by Jazmine Singh on 10/15/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

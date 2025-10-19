1964bce808daa78322c9561d6bf0a7b0f8596919
Jazmine Singh <jzmn65@gmail.com>
2025-10-19 13:22:40 -0400

    Chapter 5.8

--- ToDoLists/Views/ContentView.swift
+++ ToDoLists/Views/ContentView.swift
@@ -8,43 +8,64 @@
 import SwiftUI
 import SwiftData
 
-struct ToDoListView: View {
+enum SortOption: String, CaseIterable {
+    case asEntered = "As Entered"
+    case alphabetical = "A-Z"
+    case chronological = "Date"
+    case completed = "Not Done"
+}
+
+struct SortedToDoList: View {
     @Query var toDos: [ToDo]
     @Environment(\.modelContext) var modelContext
-    @State private var sheetIsPresented = false
+    let sortSelection: SortOption
+    
+    init(sortSelection: SortOption){
+        self.sortSelection = sortSelection
+        switch self.sortSelection {
+        case .asEntered:
+            _toDos = Query()
+        case .alphabetical:
+            _toDos = Query(sort: \.item, animation: .default)
+        case .chronological:
+            _toDos = Query(sort: \.dueDate)
+        case .completed:
+            _toDos = Query(filter: #Predicate{$0.isCompleted == false})
+        }
+    }
+    
     var body: some View {
-        NavigationStack {
-            List{
-                ForEach(toDos) { toDo in
-                    VStack(alignment: .leading) {
-                        HStack {
-                            Image(systemName: toDo.isCompleted ? "checkmark.rectangle" : "rectangle")
-                                .onTapGesture {
-                                    toDo.isCompleted.toggle()
-                                    guard let _ = try? modelContext.save() else {
-                                        print("😡 ERROR: Save after .toggel on ToDoListView did not work.")
-                                        return
-                                    }
-                                    
+        List{
+            ForEach(toDos) { toDo in
+                VStack(alignment: .leading) {
+                    HStack {
+                        Image(systemName: toDo.isCompleted ? "checkmark.rectangle" : "rectangle")
+                            .onTapGesture {
+                                toDo.isCompleted.toggle()
+                                guard let _ = try? modelContext.save() else {
+                                    print("😡 ERROR: Save after .toggel on ToDoListView did not work.")
+                                    return
                                 }
-                            NavigationLink {
-                                DetailView(toDo: toDo)
-                            } label: {
-                                Text(toDo.item)
+                                
                             }
-                        }
-                        .font(.title2)
-                        
-                        HStack{
-                            Text(toDo.dueDate.formatted(date: .abbreviated, time: .shortened))
-                                .foregroundStyle(.secondary)
-                            
-                            if toDo.reminderIsOn {
-                                Image(systemName: "calendar.badge.clock")
-                                    .symbolRenderingMode(.multicolor)
-                            }
+                        NavigationLink {
+                            DetailView(toDo: toDo)
+                        } label: {
+                            Text(toDo.item)
                         }
                     }
+                    .font(.title2)
+                    
+                    HStack{
+                        Text(toDo.dueDate.formatted(date: .abbreviated, time: .shortened))
+                            .foregroundStyle(.secondary)
+                        
+                        if toDo.reminderIsOn {
+                            Image(systemName: "calendar.badge.clock")
+                                .symbolRenderingMode(.multicolor)
+                        }
+                    }
+                }
 
 //                    .swipeActions {
 //                        Button("Delete", role: .destructive) {
@@ -53,20 +74,30 @@ struct ToDoListView: View {
 //                                print("😡 ERROR: Save after .delete on ToDoListView did not work.")
 //                                return
 //                            }
-//                            
+//
 //                        }
-                    }
-                    .onDelete { indexSet in
-                        indexSet.forEach({modelContext.delete(toDos[$0])})
-                        guard let _ = try? modelContext.save() else {
-                            print("😡 ERROR: Save after .delete on ToDoListView did not work.")
-                            return
-                        }
                 }
+                .onDelete { indexSet in
+                    indexSet.forEach({modelContext.delete(toDos[$0])})
+                    guard let _ = try? modelContext.save() else {
+                        print("😡 ERROR: Save after .delete on ToDoListView did not work.")
+                        return
+                    }
             }
+        }
+        .listStyle(.plain)
+        
+    }
+}
+
+struct ToDoListView: View {
+    @State private var sheetIsPresented = false
+    @State private var sortSelection: SortOption = .asEntered
+    var body: some View {
+        NavigationStack {
+            SortedToDoList(sortSelection: sortSelection)
             .navigationTitle("To Do List")
             .navigationBarTitleDisplayMode(.automatic)
-            .listStyle(.plain)
             .sheet(isPresented: $sheetIsPresented) {
                 NavigationStack {
                     DetailView(toDo: ToDo())
@@ -79,6 +110,14 @@ struct ToDoListView: View {
                     } label: {
                         Image(systemName: "plus")
                     }
+                }
+                ToolbarItem(placement: .bottomBar) {
+                    Picker("", selection: $sortSelection){
+                        ForEach(SortOption.allCases, id: \.self) { sortOrder in
+                            Text(sortOrder.rawValue)
+                        }
+                    }
+                    .pickerStyle(.segmented)
                 }
             }
         }
